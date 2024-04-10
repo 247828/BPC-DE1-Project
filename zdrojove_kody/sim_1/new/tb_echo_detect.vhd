b9df856ab7ac01d58d0f2a2367616a92c13b26fd
Jakub Kováč <114470358+247828@users.noreply.github.com>
2024-04-10 22:08:17 +0200

    Najnovšie otestované testbench-e

--- zdrojove_kody/sim_1/new/tb_echo_detect.vhd
+++ zdrojove_kody/sim_1/new/tb_echo_detect.vhd
@@ -1,6 +1,6 @@
 -- Testbench automatically generated online
 -- at https://vhdl.lapinoo.net
--- Generation date : 3.4.2024 20:34:12 UTC
+-- Generation date : 10.4.2024 18:22:19 UTC
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -11,31 +11,31 @@ end tb_echo_detect;
 architecture tb of tb_echo_detect is
 
     component echo_detect
-        port (trig    : in std_logic;
-              echo_in : in std_logic;
-              clk     : in std_logic;
-              rst     : in std_logic;
-              distance  : out std_logic_vector (8 downto 0));
+        port (trig     : in std_logic;
+              echo_in  : in std_logic;
+              clk      : in std_logic;
+              rst      : in std_logic;
+              distance : out std_logic_vector (8 downto 0));
     end component;
 
-    signal trig    : std_logic;
-    signal echo_in : std_logic;
-    signal clk     : std_logic;
-    signal rst     : std_logic;
-    signal distance  : std_logic_vector (8 downto 0);
+    signal trig     : std_logic;
+    signal echo_in  : std_logic;
+    signal clk      : std_logic;
+    signal rst      : std_logic;
+    signal distance : std_logic_vector (8 downto 0);
 
-    constant TbPeriod : time := 10 ns; -- EDIT Put right period here
+    constant TbPeriod : time := 10 ns; -- 100 MHz
     signal TbClock : std_logic := '0';
     signal TbSimEnded : std_logic := '0';
 
 begin
 
     dut : echo_detect
-    port map (trig    => trig,
-              echo_in => echo_in,
-              clk     => clk,
-              rst     => rst,
-              distance  => distance);
+    port map (trig     => trig,
+              echo_in  => echo_in,
+              clk      => clk,
+              rst      => rst,
+              distance => distance);
 
     -- Clock generation
     TbClock <= not TbClock after TbPeriod/2 when TbSimEnded /= '1' else '0';
@@ -47,43 +47,42 @@ begin
     begin
         -- EDIT Adapt initialization as needed
         trig <= '0';
-        echo_in <= '1';
+        echo_in <= '0';
 
         -- Reset generation
         -- EDIT: Check that rst is really your reset signal
         rst <= '1';
-        wait for 2 * TbPeriod;
+        wait for 50 ns;
         rst <= '0';
-        wait for 2 * TbPeriod;
-        -- EDIT Add stimuli here
+        wait for 50 ns;
         trig <= '1';
-        wait for 2 * TbPeriod;
+        wait for 20 ns;
         trig <= '0';
-        wait for 50000 * TbPeriod;
-        echo_in <= '0';
-        wait for 2 * TbPeriod;
+        wait for 20 ns;
         echo_in <= '1';
-        wait for 100 * TbPeriod;
-        
+        wait for 150 us;
+        echo_in <= '0';
+        wait for 20 ns;
         trig <= '1';
-        wait for 1 * TbPeriod;
+        wait for 20 ns;
         trig <= '0';
-        wait for 38145 * TbPeriod;
+        wait for 20 ns;
+        echo_in <= '1';
+        wait for 50 ns;
         rst <= '1';
-        wait for 2 * TbPeriod;
+        wait for 20 ns;
         rst <= '0';
-        wait for 25400 * TbPeriod;
+        wait for 10 ns;
         echo_in <= '0';
-        wait for 2 * TbPeriod;
-        echo_in <= '1';
-        wait for 100 * TbPeriod;
+        wait for 20 ns;
         trig <= '1';
-        wait for 2 * TbPeriod;
+        wait for 20 ns;
         trig <= '0';
-        wait for 25400 * TbPeriod;
-        echo_in <= '0';
-        wait for 2 * TbPeriod;
+        wait for 20 ns;
         echo_in <= '1';
+        wait for 150 us;
+        echo_in <= '0';
+        wait for 20 ns;
         wait for 100 * TbPeriod;
 
         -- Stop the clock and hence terminate the simulation
--- zdrojove_kody/sim_1/new/tb_trig_pulse.vhd
+++ zdrojove_kody/sim_1/new/tb_trig_pulse.vhd
@@ -1,7 +1,6 @@
-
 -- Testbench automatically generated online
 -- at https://vhdl.lapinoo.net
--- Generation date : 2.4.2024 09:38:54 UTC
+-- Generation date : 10.4.2024 16:36:09 UTC
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -12,32 +11,27 @@ end tb_trig_pulse;
 architecture tb of tb_trig_pulse is
 
     component trig_pulse
-	generic (
-		PULSE_WIDTH : integer
-        );
+        Generic (
+		  PULSE_WIDTH : integer := 1500 -- sirka pulzu: clk * PULSE_WIDTH
+		);				      -- sirka pulzu 20 us pri clk = 100 MHz
         port (start    : in std_logic;
               trig_out : out std_logic;
               clk      : in std_logic;
               rst      : in std_logic);
     end component;
 
-	--constant C_PULSE_WIDTH : integer := 10;
-
     signal start    : std_logic;
     signal trig_out : std_logic;
     signal clk      : std_logic;
     signal rst      : std_logic;
 
-    constant TbPeriod : time := 10 ns; -- EDIT Put right period here
+    constant TbPeriod : time := 10 ns; -- 100 MHz
     signal TbClock : std_logic := '0';
     signal TbSimEnded : std_logic := '0';
 
 begin
 
     dut : trig_pulse
-    generic map (
-	PULSE_WIDTH => 10
-    )
     port map (start    => start,
               trig_out => trig_out,
               clk      => clk,
@@ -54,29 +48,30 @@ begin
         -- EDIT Adapt initialization as needed
         start <= '0';
 
-         -- Reset generation
+        -- Reset generation
         -- EDIT: Check that rst is really your reset signal
         rst <= '1';
-        wait for 2 * TbPeriod;
+        wait for 50 ns;
         rst <= '0';
-        wait for 2 * TbPeriod;
+        wait for 50 ns;
+        start <= '1';
+        wait for TbPeriod;
+        start <= '0';
+        wait for 20 us;
+        start <= '1';
+        wait for TbPeriod;
+        start <= '0';
+        wait for 5 us;
+        rst <= '1';
+        wait for 50 ns;
+        rst <= '0';
+        wait for 2 us;
+        start <= '1';
+        wait for TbPeriod;
+        start <= '0';
+        wait for 20 us;
         -- EDIT Add stimuli here
-        start <= '1';
-        wait for 0.5 * TbPeriod;
-        start <= '0';
-        wait for 30 * TbPeriod;
-        start <= '1';
-        wait for 0.5 * TbPeriod;
-        start <= '0';
-        wait for 5 * TbPeriod;
-        rst <= '1';	-- reset test
-        wait for 2 * TbPeriod;
-        rst <= '0';
-        wait for 30 * TbPeriod;
-        start <= '1';
-        wait for 0.5 * TbPeriod;
-        start <= '0';
-        wait for 30 * TbPeriod;
+        wait for 100 * TbPeriod;
 
         -- Stop the clock and hence terminate the simulation
         TbSimEnded <= '1';
