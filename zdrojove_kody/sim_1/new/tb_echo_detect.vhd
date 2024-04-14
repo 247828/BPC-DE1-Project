0cac1132b71ede240fcb79b6083c736448bc9a38
Jakub Kováč <114470358+247828@users.noreply.github.com>
2024-04-14 22:25:09 +0200

    Aktualizácia zdrojových kódov simulácii

--- zdrojove_kody/sim_1/new/tb_echo_detect.vhd
+++ zdrojove_kody/sim_1/new/tb_echo_detect.vhd
@@ -1,6 +1,6 @@
 -- Testbench automatically generated online
 -- at https://vhdl.lapinoo.net
--- Generation date : 10.4.2024 18:22:19 UTC
+-- Generation date : 14.4.2024 19:47:46 UTC
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -11,20 +11,27 @@ end tb_echo_detect;
 architecture tb of tb_echo_detect is
 
     component echo_detect
+        generic (
+            DEVICE_NUMBER : INTEGER := 2
+        );
         port (trig     : in std_logic;
               echo_in  : in std_logic;
               clk      : in std_logic;
               rst      : in std_logic;
-              distance : out std_logic_vector (8 downto 0));
+              dev_num  : out std_logic_vector (2 downto 0);
+              distance : out std_logic_vector (8 downto 0);
+              status   : out std_logic);
     end component;
 
     signal trig     : std_logic;
     signal echo_in  : std_logic;
     signal clk      : std_logic;
     signal rst      : std_logic;
+    signal dev_num  : std_logic_vector (2 downto 0);
     signal distance : std_logic_vector (8 downto 0);
+    signal status   : std_logic;
 
-    constant TbPeriod : time := 10 ns; -- 100 MHz
+    constant TbPeriod : time := 10 ns; -- EDIT Put right period here
     signal TbClock : std_logic := '0';
     signal TbSimEnded : std_logic := '0';
 
@@ -35,7 +42,9 @@ begin
               echo_in  => echo_in,
               clk      => clk,
               rst      => rst,
-              distance => distance);
+              dev_num  => dev_num,
+              distance => distance,
+              status   => status);
 
     -- Clock generation
     TbClock <= not TbClock after TbPeriod/2 when TbSimEnded /= '1' else '0';
@@ -60,7 +69,7 @@ begin
         trig <= '0';
         wait for 20 ns;
         echo_in <= '1';
-        wait for 150 us;
+        wait for 5 ms;
         echo_in <= '0';
         wait for 20 ns;
         trig <= '1';
@@ -80,11 +89,10 @@ begin
         trig <= '0';
         wait for 20 ns;
         echo_in <= '1';
-        wait for 150 us;
+        wait for 20 ms;
         echo_in <= '0';
         wait for 20 ns;
         wait for 100 * TbPeriod;
-
         -- Stop the clock and hence terminate the simulation
         TbSimEnded <= '1';
         wait;
